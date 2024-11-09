9253bb3feac87fdda856350ab448c49f69ec295b
LordAndradus <LordAndradus@gmail.com>
2024-11-08 22:59:15 -0600

    Added classpath dependencies to work with Google Dagger Hilt and Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,20 @@ android {
     }
 }
 
+buildscript{
+    repositories{
+
+    }
+
+    dependencies{
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
+        classpath("com.google.gms:google-services:4.3.13")
+        val nav_version = "2.5.0"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
